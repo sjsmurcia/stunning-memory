58b57497bf448b0eb4cf2ac593d1d29f86b04200
sjsmurcia <128265045+sjsmurcia@users.noreply.github.com>
2025-06-17 16:34:45 -0600

    sjsmurcia
    theme app

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'routes.dart';
-
+import 'theme/app_theme.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
